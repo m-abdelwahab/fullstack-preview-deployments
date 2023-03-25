ef7873d811adfd3ab1c7af5920de40898a32e81b
Mahmoud Abdelwahab <hey@mahmoud.codes>
2023-03-25 13:42:03 +0200

    updates

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -2,14 +2,32 @@ import { PrismaClient } from '@prisma/client';
 
 const globalForPrisma = global as unknown as { prisma: PrismaClient };
 
+const getUrl = () => {
+  const url = process.env.DATABASE_URL;
+
+  if (!url) {
+    throw new Error('DATABASE_URL is not defined');
+  }
+
+  const hostname = url.split('@')[1].split('/')[0].split('.')[0];
+
+  const newHostname = hostname.concat('-pooler');
+
+  const newUrl = url.replace(hostname, newHostname);
+
+  // when using a pooled database connection with prisma, you need to append`?pgbouncer=true` to the connection string.
+  // The reason this is done here rather than in the .env file is because the Neon Vercel integration doesn't include it.
+  newUrl.concat('?pgbouncer=true&connect_timeout=10&pool_timeout=10');
+
+  return newUrl;
+};
+
 export const prisma =
   globalForPrisma.prisma ||
   new PrismaClient({
     datasources: {
       db: {
-        // when using a pooled database connection with prisma, you need to append`?pgbouncer=true` to the connection string.
-        // The reason this is done here rather than in the .env file is because the Neon Vercel integration doesn't include it.
-        url: `${process.env.DATABASE_URL}?pgbouncer=true&connect_timeout=10&pool_timeout=10`,
+        url: getUrl(),
       },
     },
   });
